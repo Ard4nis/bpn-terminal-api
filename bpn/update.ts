a3a493bf4de99ed20bdbc3911d00792c851498f9
Kes Lagoni Williams <kes.lagoni.williams@lego.com>
2021-01-21 09:36:11 +0100

    Updated the update function to dynamically generate the update expression

--- bpn/update.ts
+++ bpn/update.ts
@@ -2,32 +2,47 @@ import handler from "../libs/handler-lib";
 import dynamoDb from "../libs/dynamodb-lib";
 import { APIGatewayEvent, Context } from "aws-lambda";
 import { UpdateBPN } from "types";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
 
 export const main = handler(
 	async (event: APIGatewayEvent, context: Context) => {
 		const data: UpdateBPN = JSON.parse(event.body);
-		const params = {
+
+		const expression = generateUpdateQuery(data);
+
+		const params: DocumentClient.UpdateItemInput = {
 			TableName: process.env.tableName,
 			// 'Key' defines the partition key and sort key of the item to be updated
 			Key: {
 				userId: "123", // The id of the author
 				bpnId: event.pathParameters.id, // The id of the note from the path
 			},
-			// 'UpdateExpression' defines the attributes to be updated
-			// 'ExpressionAttributeValues' defines the value in the update expression
-			UpdateExpression: `SET details = :details, attachment = :attachment`,
-			ExpressionAttributeValues: {
-				":details": data.details,
-				":attachment": data.attachment,
-			},
-			// 'ReturnValues' specifies if and how to return the item's attributes,
-			// where ALL_NEW returns all attributes of the item after the update; you
-			// can inspect 'result' below to see how it works with different settings
 			ReturnValues: "ALL_NEW",
+			...expression,
 		};
+
+		console.log(params);
 
 		await dynamoDb.update(params);
 
 		return { status: true };
 	}
 );
+
+export const generateUpdateQuery = (bpnToBeUpdated: UpdateBPN) => {
+	const expression = {
+		UpdateExpression: "set",
+		ExpressionAttributeNames: {},
+		ExpressionAttributeValues: {},
+	};
+
+	Object.entries(bpnToBeUpdated).forEach(([key, item]) => {
+		expression.UpdateExpression += ` #${key} = :${key},`;
+		expression.ExpressionAttributeNames[`#${key}`] = key;
+		expression.ExpressionAttributeValues[`:${key}`] = item;
+	});
+
+	expression.UpdateExpression = expression.UpdateExpression.slice(0, -1);
+
+	return expression;
+};
