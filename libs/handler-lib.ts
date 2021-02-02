bb166ffae3ab908444011fd09964d32dfa8c7a1e
Kes Lagoni Williams <kes.lagoni.williams@lego.com>
2021-02-02 23:23:29 +0100

    Adding CORS to the API

--- libs/handler-lib.ts
+++ libs/handler-lib.ts
@@ -1,25 +1,28 @@
 import { APIGatewayEvent, Context } from "aws-lambda";
-import { Lambda } from "aws-sdk";
 import * as debug from "../services/bpn/libs/debug-lib";
 
 export default function handler(lambda) {
-	return async function (event: APIGatewayEvent, context: Context) {
-		let body, statusCode: number;
+  return async (event: APIGatewayEvent, context: Context) => {
+    let body, statusCode: number;
 
-		debug.init(event, context);
+    debug.init(event, context);
 
-		try {
-			body = await lambda(event, context);
-			statusCode = 200;
-		} catch (e) {
-			debug.flush(e);
-			body = { error: e };
-			statusCode = 500;
-		}
+    try {
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      debug.flush(e);
+      body = { error: e };
+      statusCode = 500;
+    }
 
-		return {
-			statusCode,
-			body: JSON.stringify(body),
-		};
-	};
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+    };
+  };
 }
